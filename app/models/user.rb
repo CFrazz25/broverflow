6b2fda946747ff6fefb4a6d9d0325b5aa3fec7d4
Karan Oberoi <email": null>
2017-04-06 11:35:26 -0500

    models and migrations

---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+class User < ActiveRecord::Base
+  validates :username, :email, { presence: true }
+  validates :email, { uniqueness: true }
+  has_many  :questions
+  has_many  :answers
+  has_many  :comments
+  has_many  :votes
+
+  validate :check_password
+
+  def check_password
+    if @raw_password == ""
+      @errors.add(:password, "needs to be 8 characters or more")
+    end
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by(email)
+    return user if user && user.password == password
+  end
+
+  def password
+    @password ||= BCrypt::Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @raw_password = new_password
+    @password = BCrypt::Password.create(@raw_password)
+    self.hashed_password = password
+  end
+
+end
