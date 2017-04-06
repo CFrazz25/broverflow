1efa6c887225cac3af4d21e6dca7649480050682
Chris Fraz & Tove N Mercer <pair+cfrazz25.tovemercer@devbootcamp.com>
2017-04-06 16:23:27 -0500

    answer form created on question id page, working on lost route for form

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,5 +1,6 @@
 post '/questions/:question_id/answers' do
   question = Question.find_by(id: params[:question_id])
   # question.answers.create(:)
+  new_answer = Answer.create(body: params[:body])
   redirect '/questions/:id'
 end
--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -4,7 +4,7 @@ end
 
 get '/questions/:id' do
   @question = Question.find_by(id: params[:id])
-
+  @answers = Answer.where(question_id: @question.id)
   erb :'/questions/show'
 end
 
