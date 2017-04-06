96d2d08ba2a605bd362a869adf7bcfab61a36bc2
Karan Oberoi <email": null>
2017-04-06 15:59:42 -0500

    more stuff in progress

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -3,11 +3,16 @@ get '/questions' do
 end
 
 get '/questions/new' do
-  "poop"
+  erb :'questions/new'
 end
 
 get '/questions/:id' do
 end
 
 get '/questions/:id/vote' do
+end
+
+post '/questions' do
+  @question = Question.create(title: params[:title], body: params[:body], user_id: current_user.id)
+  erb :'questions/show'
 end
