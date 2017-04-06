3619c396c6217d88a5b4554e2245e9f0aa839750
Brendan White <email": null>
2017-04-06 17:29:06 -0500

    user authentication

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,14 +1,19 @@
 get "/sessions/new" do
-  erb :'sessions/login'
+  erb :'sessions/new'
 end
 
 post "/sessions" do
-  user = User.authenticate(params[:username], params[:password])
+  user = User.authenticate(params[:email], params[:password])
   if user
-    session[:id] = user.id
+    set_user(user)
     redirect '/'
   else
     @errors = ["Invalid username or password, my broman.  Shotgun a beer and try again lol."]
-    erb :"sessions/login"
+    erb :"sessions/new"
   end
+end
+
+delete '/sessions' do
+  session[:user_id] = nil
+  redirect '/'
 end
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,3 +1,14 @@
 get "/users/new" do
-  erb :'Users/register'
+  erb :'Users/new'
+end
+
+post "/users" do
+  user = User.new(params[:user])
+  if user.save
+    set_user(user)
+    redirect "/"
+  else
+    @errors = user.errors.full_messages
+    erb :"users/new"
+  end
 end
