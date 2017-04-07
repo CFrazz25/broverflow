713a03b0b75e639c4ff564a46c37c4551e2f114a
Karan Oberoi <email": null>
2017-04-07 11:45:06 -0500

    comments for qs and as

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -8,7 +8,7 @@ end
 
 get '/questions/:id' do
   @question = Question.find_by(id: params[:id])
-  @answers = Answer.where(question_id: @question.id)
+  # @answers = Answer.where(question_id: @question.id)
   erb :'/questions/show'
 end
 
